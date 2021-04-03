cd7a30ee45cd005c6c2419cdcab35ced1a16498c
Welisson luca <welissonluca17@gmail.com>
2021-04-03 02:42:06 -0300

    feat: view new categorie add

--- controllers/categories/CategoriesController.js
+++ controllers/categories/CategoriesController.js
@@ -2,12 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-	res.send("Rota Categoria");
-});
-
-router.get("/admin/categories/new", (req, res) => {
-	res.send("Rota Categoria");
-});
+router.get('/admin/categories/new', (req, res) => {
+	res.render('admin/categories/new');
+})
 
 module.exports = router;
