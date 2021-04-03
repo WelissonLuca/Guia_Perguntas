eea45599a1c1e2e666d8fbbd580ff2f537e9c545
Welisson luca <welissonluca17@gmail.com>
2021-04-03 02:58:35 -0300

    feat: list categories view add

--- controllers/categories/CategoriesController.js
+++ controllers/categories/CategoriesController.js
@@ -18,4 +18,8 @@ router.post("/categories/save", (req, res) => {
 		});
 	else res.redirect("/admin/categories/new");
 });
+
+router.get('/admin/categories', (req, res) => {
+	res.render("admin/categories/index");
+})
 module.exports = router;
