06117d3fee81c6acfe151425fb7f6ef7d893f177
Welisson luca <welissonluca17@gmail.com>
2021-04-03 03:19:56 -0300

    feat: delete categorie

--- controllers/categories/CategoriesController.js
+++ controllers/categories/CategoriesController.js
@@ -19,10 +19,20 @@ router.post("/categories/save", (req, res) => {
 	else res.redirect("/admin/categories/new");
 });
 
-router.get('/admin/categories', (req, res) => {
- Category.findAll().then((categories) => {
-	 res.render("./admin/categories/index", { categories });
- });
-	
-})
+router.get("/admin/categories", (req, res) => {
+	Category.findAll().then((categories) => {
+		res.render("./admin/categories/index", { categories });
+	});
+});
+
+router.post("/categories/delete", (req, res) => {
+	const { id } = req.body;
+	if (id != undefined)
+		if (!isNaN(id))
+			Category.destroy({ where: { id } }).then(() =>
+				res.redirect("/admin/categories")
+			);
+		else res.redirect("/admin/categories");
+	else res.redirect("/admin/categories");
+});
 module.exports = router;
