c02069e0da34aee663571be1f68b3949e46979a9
Welisson luca <welissonluca17@gmail.com>
2021-04-03 02:47:16 -0300

    feat: categories save db for method post

--- controllers/categories/CategoriesController.js
+++ controllers/categories/CategoriesController.js
@@ -1,9 +1,21 @@
 const express = require("express");
 
 const router = express.Router();
+const Category = require("../../models/categories/Category");
+const slugify = require("slugify");
+router.get("/admin/categories/new", (req, res) => {
+	res.render("admin/categories/new");
+});
 
-router.get('/admin/categories/new', (req, res) => {
-	res.render('admin/categories/new');
-})
-
+router.post("/categories/save", (req, res) => {
+	const { title } = req.body;
+	if (title != undefined)
+		Category.create({
+			title,
+			slug: slugify(title),
+		}).then(() => {
+			res.redirect("/");
+		});
+	else res.redirect("/admin/categories/new");
+});
 module.exports = router;
