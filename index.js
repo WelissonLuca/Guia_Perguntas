0e3b0924a19b82e2af9b1c3fe904201a97dd15d3
Welisson luca <welissonluca17@gmail.com>
2021-04-04 01:50:06 -0300

    feat: list all articles for home page

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 // DB
 const connection = require("./database/database");
-//Controllers	
+//Controllers
 const categoriesController = require("./controllers/categories/CategoriesController.js");
 const articlesController = require("./controllers/articles/ArticlesController.js");
 
 //Models
-const Articles = require("./models/articles/Articles");
+const Article = require("./models/articles/Articles");
 const Category = require("./models/categories/Category");
 
 app.set("view engine", "ejs");
@@ -24,8 +24,18 @@ connection
 	.catch((err) => {
 		console.log("failed connection" + err);
 	});
-app.use('/', categoriesController);
+app.use("/", categoriesController);
 app.use("/", articlesController);
+
+app.get("/", (req, res) => {
+	Article.findAll({
+		order: [["id", "DESC"]],
+	}).then((articles) => {
+		Category.findAll().then((categories) => {
+			res.render("index", { articles: articles, categories: categories });
+		});
+	});
+});
 
 app.listen(8080, () => {
 	console.log("server is running");
