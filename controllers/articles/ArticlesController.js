65326636860cb3242313ab77084b239ec660f92d
Welisson luca <welissonluca17@gmail.com>
2021-04-04 01:08:12 -0300

    feat: list all articles

--- controllers/articles/ArticlesController.js
+++ controllers/articles/ArticlesController.js
@@ -1,12 +1,31 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
+const Category = require("../../models/categories/Category");
+const Article = require("../../models/articles/Articles");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
-	res.send("Rota Categoria");
+router.get("/admin/articles", (req, res) => {
+	Article.findAll({
+		include: [{ model: Category }],
+	}).then((articles) => {
+		res.render("admin/articles/index", { articles });
+	});
 });
 
 router.get("/admin/articles/new", (req, res) => {
-	res.render('admin/articles/new');
+	Category.findAll().then((categories) => {
+		res.render("admin/articles/new", { categories });
+	});
+});
+
+router.post("/articles/save", (req, res) => {
+	const { title, body, category } = req.body;
+	Article.create({
+		title,
+		slug: slugify(title),
+		body,
+		categoryId: category,
+	}).then(() => res.redirect("/admin/articles"));
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -1,9 +1,9 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const connection = new Sequelize('guiapress', 'root', '240306', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const connection = new Sequelize("guiapress", "root", "240306", {
+	host: "localhost",
+	dialect: "mysql",
+	timezone: "-03:00",
+});
 
 module.exports = connection;
-
