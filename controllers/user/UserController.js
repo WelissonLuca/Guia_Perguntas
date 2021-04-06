0aa29919f4d6ae5f7d787b7d529db4850ef019f3
Welisson luca <welissonluca17@gmail.com>
2021-04-06 13:55:08 -0300

    feat: save password format hash

--- controllers/user/UserController.js
+++ controllers/user/UserController.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const User = require("../../models/user/User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/user", (req, res) => {
 	res.send("Listagem de usuários");
@@ -13,8 +14,14 @@ router.get("/admin/user/create", (req, res) => {
 
 router.post("/users/create", (req, res) => {
 	const { email, password } = req.body;
+	const salt = bcryptjs.genSaltSync(10);
+	const hash = bcryptjs.hashSync(password, salt);
 
-	res.json({ email, password });
+	User.create({ email, password: hash })
+		.then(() => {
+			res.redirect("/");
+		})
+		.catch((err) => res.redirect("/"));
 });
 
 module.exports = router;
